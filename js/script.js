63bc438d9e8f46b54313dba7cc261b694a2bea13
Vladislav Murzaykin <nicochan00@mail.ru>
2022-11-24 18:04:36 +0300

    refactoring code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+// find selector
+const form = document.querySelector('#form');
+const taskInput = document.querySelector('#taskInput');
+const tasksList = document.querySelector('#tasksList');
+const emptyList = document.querySelector('#emptyList');
+
+// handler ToDo
+form.addEventListener('submit', addTask);
+
+// func ToDo
+function addTask(event) {
+    event.preventDefault();
+
+    const taskText = taskInput.value;
+
+    const taskHTML = `
+        <li class="list-group-item d-flex justify-content-between task-item">
+            <span class="task-title">${taskText}</span>
+            <div class="task-item__buttons">
+                <button type="button" data-action="done" class="btn-action">
+                    <img src="./img/tick.svg" alt="Done" width="18" height="18">
+                </button>
+                <button type="button" data-action="delete" class="btn-action">
+                    <img src="./img/cross.svg" alt="Done" width="18" height="18">
+                </button>
+            </div>
+        </li>
+    `;
+
+    tasksList.insertAdjacentHTML('beforeend', taskHTML);
+
+    taskInput.value = '';
+    taskInput.focus();
+
+    if (tasksList.children.length > 1) {
+        emptyList.classList.add('none');
+    }
+}
